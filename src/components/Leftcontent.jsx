c418437d60c0efbfad2a282802e3eba741e41253
MATHYRUBAN YATHUSAN <yathusan@MacBook-Pro-de-MATHYRUBAN.local>
2023-09-15 15:17:17 +0200

    Composant permettant d'afficher la partie de gauche de la page

---
+++ src/components/Leftcontent.jsx
@@ -0,0 +1,38 @@
+// Importation du composant Profil depuis le chemin spécifié
+import Profil from "../Chart/Profil";
+
+// Importation du composant Level depuis le chemin spécifié
+import { Level } from "../Level";
+
+// Définition de la fonction LeftContent
+function LeftContent() {
+  return (
+    <div>
+      {/* Section de profil */}
+      <div className="profil-center">
+        {/* Utilisation du composant Profil */}
+        <Profil /> {/* Composant profil */}
+      </div>
+
+      {/* Section des badges en cours, visible uniquement sur les écrans larges */}
+      <div className="mt-10 hidden lg:block">
+        {/* Libellé des badges en cours */}
+        <span className="text-white">Badges en cours:</span>
+        <div className="flex justify-center w-full">
+          <div className="relative w-80 mt-7 flex flex-wrap justify-between">
+            {/* Création de plusieurs instances du composant Level pour afficher différents badges */}
+            <Level badge={1} badgeImg={"/assets/guitare.png"} stepDone={10} />
+            <Level badge={2} badgeImg={"/assets/guitare.png"} stepDone={10} />
+            <Level badge={3} badgeImg={"/assets/guitare.png"} stepDone={10} />
+            <Level badge={4} badgeImg={"/assets/guitare.png"} stepDone={10} />
+            <Level badge={5} badgeImg={"/assets/guitare.png"} stepDone={10} />
+            <Level badge={6} badgeImg={"/assets/guitare.png"} stepDone={10} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Exportation de la fonction LeftContent en tant que composant par défaut
+export default LeftContent;
