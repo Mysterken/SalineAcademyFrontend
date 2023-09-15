3663ea8ad2c34ae3cf8ef4937703f33c62329d5f
MATHYRUBAN YATHUSAN <yathusan@MacBook-Pro-de-MATHYRUBAN.local>
2023-09-15 15:29:30 +0200

    Images page profil utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import Decouvrir from './component/decouvrir'
 import "./decouvrir.css"
 import TextWithAnimation from './component/filter'
+import Container from "./components/Container/Container";
+import { Level } from "./components/Level";
+import { useState } from "react";
 
 function App() {
 
   return (
     <>
       <Decouvrir></Decouvrir>
+      <Container />
+      <Level/>
+      <useState/>
     </>
   )
 }
