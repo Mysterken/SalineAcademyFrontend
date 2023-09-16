66f42af17e1c8618861d0e3d65511f902e2145cb
Mysterken <Mysterkenzo@gmail.com>
2023-09-16 17:51:55 +0200

    replace error page with template

--- src/renderer/_error.page.jsx
+++ src/renderer/_error.page.jsx
@@ -1,9 +1,29 @@
-export { Page }
+import {Box, Button, Typography} from '@mui/material';
+import {purple} from '@mui/material/colors';
+
+export {Page}
+
+const primary = purple[500]; // #f44336
 
 function Page() {
   return (
-    <>
-      <h1>404 Page</h1>
-    </>
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        backgroundColor: primary,
+      }}
+    >
+      <Typography variant="h1" style={{color: 'white'}}>
+        404
+      </Typography>
+      <Typography variant="h6" style={{color: 'white'}}>
+        The page you’re looking for doesn't exist.
+      </Typography>
+      <Button variant="contained" href="/home">Back Home</Button>
+    </Box>
+  );
 }
