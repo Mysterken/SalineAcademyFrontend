6f48ef96235ca7e8a6dfce5995aaac050e91ca24
Mysterken <Mysterkenzo@gmail.com>
2023-09-16 14:22:48 +0200

    add point API helper

---
+++ src/API/point.js
@@ -0,0 +1,17 @@
+import {postData, responseInvalidData, validateData} from "./dataFetcher.js";
+import {checkUserRoles, responseNoPermission} from "./user.js";
+
+const writeData = {
+  "amount": "number"
+}
+
+export async function addPoint(data) {
+  if (checkUserRoles(["ROLE_USER"])) return responseNoPermission;
+  if (validateData(writeData, data)) return responseInvalidData;
+
+  const res = await postData("/api/points", data);
+  if (res.status === 201) {
+    return res.data;
+  }
+  return res;
+}
