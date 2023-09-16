042190a91608f00cdf56556d00af75bccf3c1c25
Mysterken <Mysterkenzo@gmail.com>
2023-09-16 22:36:11 +0200

    fix navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,9 +6,11 @@ import {Box} from "@mui/material";
 
 export default function Navbar() {
   return (
-    <AppBar position="relative" style={{backgroundColor: "rgba(0, 0, 0, 0)", boxShadow: "none", margin: "16px"}}>
+    <AppBar position="relative" sx={{backgroundColor: "rgba(0, 0, 0, 0)", boxShadow: "none", marginTop: "16px"}}>
       <Toolbar>
-        <LogoDev style={{marginRight: "auto"}}/>
+        <a href="/home" style={{marginRight: "auto"}}>
+          <LogoDev sx={{fill: "black"}}/>
+        </a>
         <Box p="16px 32px" style={{
           display: "flex",
           gap: "48px",
@@ -17,32 +19,32 @@ export default function Navbar() {
           fontSize: "14px",
           fontWeight: "1000",
         }}>
-          <Box style={{display: "flex", alignItems: "center"}}>
-            <Home style={{marginRight: "15px"}}/>
+          <Box sx={{display: "flex", alignItems: "center"}}>
+            <Home sx={{marginRight: "15px"}}/>
             <Typography color="inherit" noWrap>
               HOME
             </Typography>
           </Box>
-          <Box style={{display: "flex", alignItems: "center"}}>
-            <Tv style={{marginRight: "15px"}}/>
+          <Box sx={{display: "flex", alignItems: "center"}}>
+            <Tv sx={{marginRight: "15px"}}/>
             <Typography noWrap>
               WATCH
             </Typography>
           </Box>
           <Box style={{display: "flex", alignItems: "center"}}>
-            <MusicNote style={{marginRight: "15px"}}/>
+            <MusicNote sx={{marginRight: "15px"}}/>
             <Typography noWrap>
               LISTEN
             </Typography>
           </Box>
           <Box style={{display: "flex", alignItems: "center"}}>
-            <School style={{marginRight: "15px"}}/>
+            <School sx={{marginRight: "15px"}}/>
             <Typography noWrap>
               LEARN
             </Typography>
           </Box>
         </Box>
-        <Person style={{marginLeft: "auto"}}/>
+        <Person sx={{fill: "black", marginLeft: "auto"}}/>
       </Toolbar>
     </AppBar>
   )
