7b2c3c02b0cd4cfbc3feef66f3dfb7300294fde9
MATHYRUBAN YATHUSAN <yathusan@MacBook-Pro-de-MATHYRUBAN.local>
2023-09-17 14:06:10 +0200

    Modifs conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
 // import "./decouvrir.css"
 import Container from "./components/Container/";
 import { Level } from "./components/Level";
@@ -11,27 +10,6 @@ function App() {
       <Container />
       <Level/>
       <useState/>
-=======
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home.jsx';
-
-
-
-function App() {
-  
-  const [count, setCount] = useState(0)
-
-  return (
-
-    <>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
-
->>>>>>> 5c0991139eeee72ba845649695f4732345e0bcc0
     </>
   )
 }
