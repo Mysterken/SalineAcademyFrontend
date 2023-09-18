5aefa9cd0f95ef33cfaca729ac955eecde2c4398
Mysterken <Mysterkenzo@gmail.com>
2023-09-18 13:38:05 +0200

    add URL and roboto font to use

--- src/renderer/_default.page.client.jsx
+++ src/renderer/_default.page.client.jsx
@@ -2,11 +2,16 @@ export {render}
 
 import {hydrateRoot} from 'react-dom/client'
 import "../style/base.css"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 async function render(pageContext) {
   const {Page} = pageContext
   hydrateRoot(
     document.getElementById('page-view'),
-    <Page/>
+    <Page url={window.location.href} />
   )
 }
